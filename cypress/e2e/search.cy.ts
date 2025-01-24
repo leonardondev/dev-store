2e5518148d37a49433e12da027219c3fe72395be
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2025-01-23 23:08:14 -0300

    test: setup cypress e2e tests

---
+++ cypress/e2e/search.cy.ts
@@ -0,0 +1,16 @@
+describe('search products', () => {
+  it('should be able to search for products', () => {
+    cy.searchByQuery('moletom')
+
+    cy.location('pathname', { timeout: 5000 }).should('include', '/search')
+    cy.location('search').should('include', 'q=moletom')
+
+    cy.get('a[href^="/product"]').should('exist')
+  })
+
+  it('should not be able to visit search page without a search query', () => {
+    cy.visit('/search')
+
+    cy.location('pathname', { timeout: 5000 }).should('equal', '/')
+  })
+})
